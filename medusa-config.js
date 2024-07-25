ae786a9f24eac7e647675c741ecbea3e0c96a529
Shahed Nasser <shahednasser@gmail.com>
2024-07-25 12:52:42 +0300

    change path

--- medusa-config.js
+++ medusa-config.js
@@ -17,7 +17,8 @@ module.exports = defineConfig({
   },
   admin: {
     disable: process.env.DISABLE_MEDUSA_ADMIN === "true",
-    backendUrl: process.env.MEDUSA_BACKEND_URL
+    backendUrl: process.env.MEDUSA_BACKEND_URL,
+    path: "/"
   },
   modules: {
     [Modules.CACHE]: {
